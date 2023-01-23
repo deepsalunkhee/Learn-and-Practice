75c7fe9960d110ae1574bb24c3e55bc3b1a4991d
deepsalunkhee <99858248+deepsalunkhee@users.noreply.github.com>
2023-01-23 22:13:49 +0530

    bubblesort

--- SEM-4/AOA/Practcal1/bubblesort.c
+++ SEM-4/AOA/Practcal1/bubblesort.c
@@ -7,7 +7,7 @@ void bubblesort(int a[],int size)
     int i,j,temp;
     for(int i=0;i<size-1;i++)
     {
-        for(int j=0;j<size-i-1;j++)
+        for(int j=0;j<size-1;j++)
         {
             if(a[j]>a[j+1])
             {
@@ -28,19 +28,37 @@ int main()
     int *a;
     FILE *arr;
     FILE *arrsort;
+
+
     a=(int *)malloc(sizeof(int)*size);
+    arr=fopen("E:\\GIt\\SEM-4\\AOA\\Practcal1\\arr.txt","w");
+
+
+    for( i=0;i<size;i++)
+    fprintf(arr,"%d\n",(rand()));
+
+    fclose(arr);
+
     arr=fopen("E:\\GIt\\SEM-4\\AOA\\Practcal1\\arr.txt","r");
     for( i=0;i<size;i++)
     fscanf(arr,"%d",&a[i]);
-    t = clock();
+
+    
+
+    clock_t start = clock();
     bubblesort(a,size);
-    t = clock() - t;
-    // for( i=0;i<size;i++)
-    // printf("%d\t",a[i]);
-    arr=fopen("E:\\GIt\\SEM-4\\AOA\\Practcal1\\arrsort.txt","w");
-    for( i=0;i<size;i++)
-    fprintf(arr,"%d\n",a[i]);
-    double timetosort = ((double)t)/CLOCKS_PER_SEC;
-    printf("%d",timetosort);
+    clock_t end = clock();
+
+
+    arrsort=fopen("E:\\GIt\\SEM-4\\AOA\\Practcal1\\arrsort.txt","w");
+    for( int i=0;i<size;i++)
+    fprintf(arrsort,"%d\n",a[i]);
+
+
+    
+    
+    
+    double timetosort = ((double)(end-start))/CLOCKS_PER_SEC;
+    printf("%ld",timetosort);
     return 0;
 }
